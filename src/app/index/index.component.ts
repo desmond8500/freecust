284d07692fe9624a5518676bba75f91f71be59f3
kratos <desmond8500@gmail.com>
2024-02-08 02:20:57 +0000

    update

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -10,8 +10,35 @@ import { Component } from '@angular/core';
 })
 export class IndexComponent {
   websites = [
-    { name: 'Freecust Mobile', link: 'https://freecust.netlify.app/', description: "", img: '../../assets/img/internet.png'},
-    { name: 'Freecust server', link: 'https://freecustserver.yonkou.info', description: "", img: '../../assets/img/internet.png'},
-    { name: 'Freecust Web', link: 'https://freecustweb.netlify.app', description: "", img: '../../assets/img/internet.png'},
+    {
+      name: 'Freecust Mobile',
+      link: 'https://freecust.netlify.app/',
+      description: "",
+      img: '../../assets/img/internet.png'},
+    {
+      name: 'Freecust server',
+      link: 'https://freecustserver.yonkou.info',
+      description: "",
+      img: '../../assets/img/internet.png'},
+    {
+      name: 'Freecust Web',
+      link: 'https://freecustweb.netlify.app',
+      description: "",
+      img: '../../assets/img/internet.png'},
+    {
+      name: 'Termii',
+      link: 'https://termii.com/',
+      description: "",
+      img: '../../assets/img/internet.png'},
+    {
+      name: 'Stripe',
+      link: 'https://stripe.com/',
+      description: "",
+      img: '../../assets/img/internet.png'},
+    {
+      name: 'Stripe API DOCS',
+      link: 'https://stripe.com/docs/api',
+      description: "",
+      img: '../../assets/img/internet.png'},
   ]
 }
