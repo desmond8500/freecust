b2d393c50873b5266bf0066b128a220803b02138
kratos <desmond8500@gmail.com>
2024-02-08 02:35:48 +0000

    update

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -14,31 +14,67 @@ export class IndexComponent {
       name: 'Freecust Mobile',
       link: 'https://freecust.netlify.app/',
       description: "",
-      img: '../../assets/img/internet.png'},
+      img: '../../assets/img/internet.png'
+    },
     {
       name: 'Freecust server',
       link: 'https://freecustserver.yonkou.info',
       description: "",
-      img: '../../assets/img/internet.png'},
+      img: '../../assets/img/internet.png'
+    },
     {
       name: 'Freecust Web',
       link: 'https://freecustweb.netlify.app',
       description: "",
-      img: '../../assets/img/internet.png'},
+      img: '../../assets/img/internet.png'
+    },
     {
       name: 'Termii',
       link: 'https://termii.com/',
       description: "",
-      img: '../../assets/img/internet.png'},
+      img: '../../assets/img/internet.png'
+    },
     {
       name: 'Stripe',
       link: 'https://stripe.com/',
       description: "",
-      img: '../../assets/img/internet.png'},
+      img: '../../assets/img/internet.png'
+    },
     {
       name: 'Stripe API DOCS',
       link: 'https://stripe.com/docs/api',
       description: "",
-      img: '../../assets/img/internet.png'},
+      img: '../../assets/img/internet.png'
+    },
+    {
+      name: 'FIGMA',
+      link: 'https://www.figma.com/file/XeSdFGQWhxISqucvw4bKuQ/FREECUST?type=design&node-id=0-1&mode=design&t=oRo9qBORAgnsRVfv-0',
+      description: "",
+      img: '../../assets/img/internet.png'
+    },
+    {
+      name: 'Ionic Docs',
+      link: 'https://ionicframework.com/docs/',
+      description: "",
+      img: '../../assets/img/internet.png'
+    },
+    {
+      name: 'Angular Docs',
+      link: 'https://angular.dev/',
+      description: "",
+      img: '../../assets/img/internet.png'
+    },
+    {
+      name: 'Fiche de parcours',
+      link: 'https://docs.google.com/spreadsheets/d/11VvF4AQlNKXIN46QThhTxHKa0kTgj13h/edit#gid=1577116275',
+      description: "",
+      img: '../../assets/img/internet.png'
+    },
+    {
+      name: 'Gitmab',
+      link: 'https://about.gitlab.com/',
+      description: "",
+      img: '../../assets/img/internet.png'
+    },
   ]
 }
