5aa38cb40516c75cd9bd08b18ff8432cedbbd0ad
kratos <desmond8500@gmail.com>
2024-02-09 16:55:36 +0000

    update

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -13,67 +13,73 @@ export class IndexComponent {
     {
       name: 'Freecust Mobile',
       link: 'https://freecust.netlify.app/',
-      description: "",
+      description: "Application mobile",
       img: '../../assets/img/internet.png'
     },
     {
       name: 'Freecust server',
       link: 'https://freecustserver.yonkou.info',
-      description: "",
+      description: "Backend",
       img: '../../assets/img/internet.png'
     },
     {
       name: 'Freecust Web',
       link: 'https://freecustweb.netlify.app',
-      description: "",
+      description: "Application web",
       img: '../../assets/img/internet.png'
     },
     {
       name: 'Termii',
       link: 'https://termii.com/',
-      description: "",
+      description: "Fournisseur OTP",
       img: '../../assets/img/internet.png'
     },
     {
       name: 'Stripe',
       link: 'https://stripe.com/',
-      description: "",
+      description: "API de paiment",
       img: '../../assets/img/internet.png'
     },
     {
       name: 'Stripe API DOCS',
       link: 'https://stripe.com/docs/api',
-      description: "",
+      description: "Documentation de l'API de paiement",
       img: '../../assets/img/internet.png'
     },
     {
       name: 'FIGMA',
       link: 'https://www.figma.com/file/XeSdFGQWhxISqucvw4bKuQ/FREECUST?type=design&node-id=0-1&mode=design&t=oRo9qBORAgnsRVfv-0',
-      description: "",
+      description: "Design",
       img: '../../assets/img/internet.png'
     },
     {
       name: 'Ionic Docs',
       link: 'https://ionicframework.com/docs/',
-      description: "",
+      description: "Docs IONIC",
       img: '../../assets/img/internet.png'
     },
     {
       name: 'Angular Docs',
       link: 'https://angular.dev/',
-      description: "",
+      description: "Doc Angular",
       img: '../../assets/img/internet.png'
     },
     {
       name: 'Fiche de parcours',
       link: 'https://docs.google.com/spreadsheets/d/11VvF4AQlNKXIN46QThhTxHKa0kTgj13h/edit#gid=1577116275',
-      description: "",
+      description: "Fiche excel de tests",
       img: '../../assets/img/internet.png'
     },
     {
-      name: 'Gitmab',
+      name: 'Gitlab',
       link: 'https://about.gitlab.com/',
-      description: "",
+      description: "Herbergeur des Répos",
+      img: '../../assets/img/internet.png'
+    },
+    {
+      name: 'Init Server',
+      link: 'https://freecustinit.yonkou.info/',
+      description: "Interface de configuration des données de test",
       img: '../../assets/img/internet.png'
     },
   ]
